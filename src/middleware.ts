c09ea12e0977decc5461f763513764639a207ae5
rizalas21 <sudrajatrizal1@gmail.com>
2026-03-17 22:21:37 +0700

    feat(auth): add multi-role support (in progress)

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,13 +5,28 @@ import { NextRequest, NextResponse } from "next/server";
 export default async function middleware(req: NextRequest) {
   const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
   const url = await req.nextUrl.pathname;
+
   if (url.includes("/api/register") || url.includes("/api/auth"))
     return NextResponse.next();
 
   if (!token) return NextResponse.redirect(new URL("/signin", req.url));
+  const role = token.role as string | undefined;
 
   if (typeof token.exp === "number" && Date.now() / 1000 > token.exp) {
     return NextResponse.redirect(new URL("/signin?expired=true", req.url));
+  }
+
+  const reqPath = req.nextUrl.pathname;
+
+  if (
+    reqPath.startsWith("/dashboard") ||
+    reqPath.startsWith("/goods") ||
+    reqPath.startsWith("/units") ||
+    reqPath.startsWith("/users")
+  ) {
+    if (role?.toLocaleLowerCase() !== "admin") {
+      return NextResponse.redirect(new URL("/403", req.url));
+    }
   }
 
   const requestHeaders = new Headers(req.headers);
